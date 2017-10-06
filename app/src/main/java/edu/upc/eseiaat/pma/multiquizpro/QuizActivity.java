c9134162738295ebb6f9e3915cf2134b0d31cd6e
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-06 16:09:16 +0200

    Crear quadre de diàleg (amb strings traduïts) amb l'opció d'acabar

--- app/src/main/java/edu/upc/eseiaat/pma/multiquizpro/QuizActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/multiquizpro/QuizActivity.java
@@ -1,5 +1,7 @@
 package edu.upc.eseiaat.pma.multiquizpro;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -46,81 +48,100 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 checkAnswer();
 
-                if (current_question < all_questions.length-1) {
+                if (current_question < all_questions.length - 1) {
                     current_question++;
-                    showQuestion();}
+                    showQuestion();
+                } else {
+                    checkResults();
+                }
 
-                else {
-                    int correct = 0, incorrect = 0;
-                    for (boolean b : answer_is_correct) {
-                        if (b) correct++;
-                        else incorrect++; }
+                btn_prev.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        checkAnswer();
+                        if (current_question > 0) {
+                            current_question--;
+                            showQuestion();
+                        }
+                    }
+                });
+            }});}
 
+            private void checkAnswer() {
+                int id = group.getCheckedRadioButtonId();
+                int current_answer = -1;
 
-                    String result = String.format("correct: %d, incorrect: %d", correct, incorrect);
-                    //String result = String.format("%s: %d, %s: %d", R.string.correct, correct, R.string.incorrect, incorrect);
+                for (int i = 0; i < ids_answers.length; i++) {
+                    if (ids_answers[i] == id) {
+                        current_answer = i;
+                    }
 
-                    Toast.makeText(QuizActivity.this, result, Toast.LENGTH_LONG).show();
-                    finish(); }
-
-                for (int i = 0; i < answer_is_correct.length; i++) {
-                    Log.i("results", String.format("Answer %d: %d (%b)", i, answer[i], answer_is_correct[i])); }}
-        });
-
-        btn_prev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer();
-                if (current_question > 0) {
-                    current_question--;
-                    showQuestion(); }
+                    answer_is_correct[current_question] = (current_answer == correct_answer);
+                    answer[current_question] = current_answer;
+                }
             }
-        });
-    }
 
-    private void checkAnswer() {
-        int id = group.getCheckedRadioButtonId();
-        int current_answer = -1;
+            private void showQuestion() {
+                String q = all_questions[current_question];
+                String[] parts = q.split(";");
 
-        for (int i = 0; i < ids_answers.length; i++) {
-            if (ids_answers[i] == id) {
-                current_answer = i; }  }
+                group.clearCheck();
+                text_question.setText(parts[0]);
 
-        answer_is_correct[current_question] = (current_answer == correct_answer);
-        answer[current_question] = current_answer;  }
+                for (int i = 0; i < ids_answers.length; i++) {
+                    RadioButton rb = (RadioButton) findViewById(ids_answers[i]);
+                    String current_answer = parts[i + 1];
+                    if (current_answer.charAt(0) == '*') {
+                        correct_answer = i;
+                        current_answer = current_answer.substring(1);}
 
-    private void showQuestion() {
-        String q = all_questions[current_question];
-        String[] parts = q.split(";");
+                    rb.setText(current_answer);
 
-        group.clearCheck();
-        text_question.setText(parts[0]);
-
-        for (int i = 0; i < ids_answers.length; i++) {
-            RadioButton rb = (RadioButton) findViewById(ids_answers[i]);
-            String current_answer = parts[i+1];
-            if (current_answer.charAt(0) == '*') {
-                correct_answer = i;
-                current_answer = current_answer.substring(1);  }
-
-            rb.setText(current_answer);
-
-            if (answer[current_question] == i) {
-                rb.setChecked(true); }  }
+                    if (answer[current_question] == i) {
+                        rb.setChecked(true);}}
 
 
-        if (current_question == 0) {
-            btn_prev.setVisibility(View.GONE);}
+                if (current_question == 0) {
+                    btn_prev.setVisibility(View.GONE);
+                } else {
+                    btn_prev.setVisibility(View.VISIBLE);
+                }
 
-        else {
-            btn_prev.setVisibility(View.VISIBLE); }
+                if (current_question == all_questions.length - 1) {
+                    btn_next.setText(R.string.check);
+                } else {
+                    btn_next.setText(R.string.next);
+                }
+            }
 
-        if (current_question == all_questions.length-1) {
-            btn_next.setText(R.string.finish);}
+            private void checkResults() {
+                int correct = 0, incorrect = 0, unanswered = 0;
+                for (int i=0; i< all_questions.length; i++) {
+                    if (answer_is_correct[i]) correct++;
+                    else { if (answer[i] == -1 )unanswered++;
+                        else incorrect++;}
+                }
 
-        else {
-            btn_next.setText(R.string.next); }
-    }
+                /*String message = getString(R.string.correct) + ": " + correct +
+                        getString(R.string.incorrect) + ": " + incorrect +
+                        getString(R.string.unanswered) + ": " + unanswered;*/
+
+               String message = String.format("%s: %d\n%s: %d\n%s: %d\n", getString(R.string.correct),
+                       correct, getString(R.string.incorrect), incorrect, getString(R.string.unanswered),unanswered);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.results);
+                builder.setMessage(message);
+                builder.setPositiveButton(R.string.finish, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                builder.create().show();
+
+            }
 
 
 }
